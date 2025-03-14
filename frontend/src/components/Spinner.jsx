3aa872d9747df314e164e9b3eb4b737a22801b77
Haronzie <haronzie2002@gmail.com>
2025-03-14 23:07:20 +0800

    Deleted

--- frontend/src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Spinner.css'; // Create a CSS file for spinner styles
-
-const Spinner = () => {
-  return <div className="spinner" aria-label="Loading"></div>;
-};
-
-export default Spinner;
